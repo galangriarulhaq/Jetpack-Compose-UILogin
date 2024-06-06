f54d58f9951d004efc4e29fdee9d6822a0d47077
galangriarulhaq <galangrz337@gmail.com>
2024-06-06 22:00:40 +0700

    add TextField

--- app/src/main/java/com/example/test_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/test_jetpack_compose/MainActivity.kt
@@ -6,24 +6,39 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+//import androidx.compose.foundation.horizontalScroll
+//import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 //import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 //import androidx.compose.foundation.layout.padding
 //import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,22 +76,58 @@ fun Login() {
             fontStyle = FontStyle.Italic,
             fontWeight = FontWeight.Bold,
             color = Color(android.graphics.Color.parseColor("#7d32a8")))
+        var user by remember { mutableStateOf("username")}
+        var pass by remember { mutableStateOf("password")}
+        var passwordVisible by rememberSaveable() { mutableStateOf(false)}
+        TextField(value = user, {text -> user=text},
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(66.dp)
+                .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
+                .border(
+                    1.dp, Color(android.graphics.Color.parseColor("#7d32a8")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle( textAlign = TextAlign.Center,
+                    color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                    fontSize = 14.sp
+                ),
+
+            )
+        TextField(value = pass, {text -> pass=text},
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Transparent)
+                .height(66.dp)
+                .padding(start = 64.dp, end = 64.dp, top = 8.dp, bottom = 8.dp)
+                .border(
+                    1.dp, Color(android.graphics.Color.parseColor("#7d32a8")),
+                    shape = RoundedCornerShape(50)
+                ),
+            shape = RoundedCornerShape(50),
+            textStyle = TextStyle( textAlign = TextAlign.Center,
+                color = Color(android.graphics.Color.parseColor("#7d32a8")),
+                fontSize = 14.sp
+                ),
+
+            )
 
     }
 }
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//
+//}
 
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Test_Jetpack_ComposeTheme {
-        Greeting("Galang")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Test_Jetpack_ComposeTheme {
+//        Greeting("Galang")
+//    }
+//}
